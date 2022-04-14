7c46d04ad564d8c6ded2b9ee309c76a9792c1980
Zlatko <iliev.zlatko@gmail.com>
2022-04-14 17:41:57 +0300

    setup lerna to run all packages with dev command

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Button } from "@ds.e/react/lib";
+import "@ds.e/scss/lib/Button.css";
+
+ReactDOM.render(
+  <Button label="example button" />,
+  document.getElementById("root")
+);
